9e189473a1554daf139c6a47eaebf53ca17ec2e5
Rushabh <rs@rushabhs.com>
2016-09-12 10:39:24 -0700

    setup entry file. setup webpack for es6 syntax

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ module.exports = {
   output: {
     filename: "./lib/bundle.js"
   },
-  devtool: 'source-maps',
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ["es2015"]
+        }
+      }
+    ]
+  },
   resolve: {
     extensions: ["", ".js" ]
   }
